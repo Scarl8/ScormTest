a3cf32ae206e01d86360f311922d99c128e95032
Scarl8 <109643738+Scarl8@users.noreply.github.com>
2022-07-20 09:48:06 +0530

    Add files via upload

---
+++ rxd/configuration.js
@@ -0,0 +1,17 @@
+var RXDConfig = {
+    // the entry URL to the content
+    contentUrl: "https://scormtestacct.ai.emtrain.com/scorm/programs/1979?API_KEY=f72deb3f54a5452db540c235d594a5dd&INT_KEY=965a99f695ca41fcaf044d2e548207b8",
+
+    // value to use as the HTML title for the proxy package
+    courseTitle: "Emtrain AI SCORM Test Course",
+
+    // URL where the RXD files are located and available via http/s
+    rxdHostUrl: "https://scormtestacct.ai.emtrain.com/scorm-rustici/rxd",
+
+    // the path to the content API wrapper page, appended to the rxdHostUrl
+    // unless it is an absolute http/s URL
+    contentApi: "contentAPI.html",
+
+    // URL to fetch pre-launch configuration settings from
+    preLaunchConfigurationUrl: ""
+  };
